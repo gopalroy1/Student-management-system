adefda1e80890f639523dfda1ba42f239eabe948
Gopal <gopalroy5397@gmail.com>
2022-12-06 09:26:12 +0530

    Initial commit

---
+++ src/GUI/jOptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author Gopal Roy
+ */
+class jOptionPane {
+
+    static void ShowMesssageDialog(Object object, String username_or_password_is_inncorrect) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
